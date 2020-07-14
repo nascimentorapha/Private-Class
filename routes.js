d3afea4e150bf47086b7198fcd4febd45c239b11
nascimentorapha <nascimento.rapha@hotmail.com>
2020-07-14 16:34:53 -0300

    created properly students route

--- routes.js
+++ routes.js
@@ -1,31 +1,24 @@
 const express = require('express')
 const routes = express.Router()
-const teachers = require('./teachers.js')
+const teachers = require('./controllers/teachers.js')
+const students = require('./controllers/students.js')
 
 routes.get("/", function(req, res){
     return res.redirect("/teachers")
 })
-
 routes.get('/teachers', teachers.index)
-
 routes.get('/teachers/create', function (req, res){
-
     return res.render("teachers/create")
 })
-
 routes.get('/teachers/:id', teachers.show)
-
 routes.get('/teachers/:id/edit', teachers.edit)
-
 routes.post('/teachers', teachers.post)
-
 routes.put('/teachers', teachers.put)
-
 routes.delete('/teachers', teachers.delete)
 
-routes.get('/members', function (req, res){
-    return res.send("members")
-})
+
+routes.get('/students', students.index)
+
 
 routes.use(function(req, res) {
     res.status(404).render("not-found");
